ff2a1adde430d0f2dc7f4fbeaf867d49e415049a
Nikita <nkdevinfo@gmail.com>
2021-05-30 13:33:10 +0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,8 @@
-console.log('test');
+if (typeof tocbot !== 'undefined' {
+  tocbot.init({
+    tocSelector: '.main-content__toc',
+    contentSelector: '.main-content__text',
+    headingSelector: 'h1, h2, h3',
+    hasInnerContainers: true,
+  });
+}
